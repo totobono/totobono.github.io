e52ad8e29467d6a2675cf38cf124571734089548
Salvatore Bono <salvatore.a.bono@accenture.com>
2022-11-16 16:23:11 +0100

    first commit

---
+++ docs/build/markdown/index.md
@@ -0,0 +1,18 @@
+<!-- JTI Datamesh Framework documentation master file, created by
+sphinx-quickstart on Wed Nov 16 12:57:59 2022.
+You can adapt this file completely to your liking, but it should at least
+contain the root `toctree` directive. -->
+# Welcome to JTI Datamesh Framework’s documentation!
+
+**NOTE**: This project is under active development.
+
+# Indices and tables
+
+
+* [Index](genindex.md)
+
+
+* [Module Index](py-modindex.md)
+
+
+* [Search Page](search.md)
---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import pathlib
+import sys
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'JTI Datamesh Framework'
+copyright = '2022, Salvatore Bono'
+author = 'Salvatore Bono'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+   'sphinx.ext.autosummary',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinxdoc'
+html_static_path = ['_static']
